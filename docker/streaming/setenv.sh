daa9962b1bfc683a7e981a4d8c9ff046cc4941ee
yannael <yannael@gmail.com>
2016-05-13 16:46:33 +0200

    Initial commit

---
+++ docker/streaming/setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+alias notebook="jupyter notebook --ip='*'"
+
+export PATH=/home/guest/spark/bin:home/guest/spark/sbin:home/guest/anaconda2/bin:$PATH
+
+export SPARK_HOME=/home/guest/spark
+
+export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
+export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.9-src.zip:$PYTHONPATH
+
---
+++ docker/streaming/startup_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#export JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk.x86_64"
+
+service sshd start
