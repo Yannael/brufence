66c9aadb0bcff3f65aedf5afff864e848ffc8dbc
yannael <yannael@gmail.com>
2016-05-16 13:26:59 +0200

    Added set

--- docker/streaming/setenv.sh
+++ docker/streaming/setenv.sh
@@ -10,3 +10,6 @@ export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
 export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.9-src.zip:$PYTHONPATH
 
 export JAVA_HOME=/etc/alternatives/java_sdk
+
+export PATH=$HOME/sbt/bin:$PATH
+
